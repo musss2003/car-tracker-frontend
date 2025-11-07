0083722207563801315ff26c69343659f697bcc7
Mustafa Sinanovic <mustafasinanovic2003@gmail.com>
2025-11-07 13:12:56 +0100

    Remove proxy - using direct HTTPS with free SSL certificate

--- api/test.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-  runtime: 'nodejs',
-};
-
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Test function works!', url: req.url, query: req.query });
-}
